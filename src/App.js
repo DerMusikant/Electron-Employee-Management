dad74bab9fa605708d0e260e4247d5809cfc19eb
DerMusikant <rivettivalentin@gmail.com>
2022-12-20 20:14:16 -0300

    Changed folder layout, added photon styles and updated the README, project ready to start!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="window">
+        <div class="window-content">
+          <div class="pane-group">
+            <div class="pane">First Panel</div>
+            <div class="pane">Second Panel</div>
+            <div class="pane">Third Panel</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
